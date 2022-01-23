97989a08e1327f75ac54e847598a13bf6786fc1d
Tadeusz Kondracki <tadeusz.kondracki@gmail.com>
2022-01-23 17:05:12 +0100

    cleanup 2

--- main.c
+++ main.c
@@ -3,10 +3,8 @@
 #include "geotrace.h"
 
 int main() {
-	t_line	ray = {1, 0, 0,
-				0, 0, 0};
-	t_plane	plane = {-5, 1, 1,
-				0, 1, 0};
+	t_line	ray = {{1, 0, 0}, { 0, 0, 0 }};
+	t_plane	plane = {{-5, 1, 1}, {0, 1, 0}};
 
 	printf("intersect distance: %f\n", plane_intersect(&plane, ray));
 
